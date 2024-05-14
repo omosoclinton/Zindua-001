731b4eeb94fa522030d87d35ea3f489bec83b184
omosoclinton <clintonomoso@gmail.com>
2024-05-13 22:55:00 -0400

    Improvement

--- WK-2/area.js
+++ WK-2/area.js
@@ -1,23 +1,26 @@
-// {
-//     let width, length, area;
+// Area of a rectangle
+{
+    let width, length, area;
 
-//     width = 10;
-//     length = 20;
-//     area = width * length;
-//     console.log(area);
-// }
+    width = 10;
+    length = 20;
+    area = width * length;
+    console.log(area);
+}
 
-// {
-//     const pi = 3.142;
-//     let d, area, r;
-//     d = 14;
-//     r = d/2;
-//     area = pi * r ** 2
-//     console.log(r);
-//     console.log(area);   
-// }
+// Area of a circle
+{
+    const pi = 3.142;
+    let d, area, r;
+    d = 14;
+    r = d/2;
+    area = pi * r ** 2
+    console.log(r);
+    console.log(area);   
+}
 
 
+// Increment -- post and pre
 let a, b, c;
 
 a = 3;
